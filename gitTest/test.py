814ca6b9a3756096e1c3682dbd29ffb3d7532a87
Jacob Wong <jacob.wong1606@gmail.com>
2026-01-17 13:28:15 -0500

    new print statements

--- gitTest/test.py
+++ gitTest/test.py
@@ -1,1 +1,10 @@
 print("hello this is a test message to get better with git and github commands")
+
+
+print("i have now cloned the repo from git and working on it using WSL. now i will push it back to github")
+
+print(" I will make a commit mesage in github so i know what i did")
+
+
+
+# I JUST WROTE NEW CODE HERE HAHAHAHAH
